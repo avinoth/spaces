cdb78259a02a5e562ed63730512b744fcebb1ded
vishaldeepak <vishal.deepak@outlook.com>
2016-05-26 09:51:02 +0530

    Added session when user is logged in

--- web/controllers/session_controller.ex
+++ web/controllers/session_controller.ex
@@ -8,7 +8,7 @@ defmodule Spaces.SessionController do
 
   def new(conn, %{"code" => code}) do
     url_params = %{client_id: "35296394051.35346594944", client_secret: "b19768c064c2825465b668bf74e7b46b", redirect_uri: session_url(conn, :new)}
-    get_url = %URI{path: "https://slack.com/api/oauth.access", query: URI.encode_query(url_params)}
+      get_url = %URI{path: "https://slack.com/api/oauth.access", query: URI.encode_query(url_params)}
     |> URI.to_string
     case HTTPoison.get(get_url) do
       {:ok, %HTTPoison.Response{status_code: status_code, body: body}} ->
@@ -19,7 +19,7 @@ defmodule Spaces.SessionController do
             conn = put_flash(conn, :error, "Error in repsone that was received from slack")
           _ ->
             [_,{_,access_token},_,{_,user_id},{_,team_name},{_,team_id}] = body
-            conn = put_session(conn, :team_name, team_name)
+            conn = put_session(conn, :user, [team_name: team_name, team_id: team_id, user_id: user_id])
         end
       {:error, %HTTPoison.Error{reason: reason}} ->
           conn = put_flash(conn, :error, "Bad Request sent to slack")
