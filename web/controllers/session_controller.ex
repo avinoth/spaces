8ceb34213b0e1ddb409a73b2c96ad9d22f788fe3
vinoth <a.vinothk@outlook.com>
2016-06-06 08:43:46 +0530

    Move client id and client secret to env var

--- web/controllers/session_controller.ex
+++ web/controllers/session_controller.ex
@@ -6,9 +6,10 @@ defmodule Spaces.SessionController do
   end
 
   def new(conn, %{"code" => code}) do
-    url_params = %{client_id: "35296394051.35346594944", client_secret: "b19768c064c2825465b668bf74e7b46b", redirect_uri: session_url(conn, :new)}
-      get_url = %URI{path: "https://slack.com/api/oauth.access", query: URI.encode_query(url_params)}
+    url_params = %{client_id: System.get_env("SLACK_CLIENT_ID"), client_secret: System.get_env("SLACK_CLIENT_SECRET"), redirect_uri: session_url(conn, :new)}
+    get_url = %URI{path: "https://slack.com/api/oauth.access", query: URI.encode_query(url_params)}
     |> URI.to_string
+
     case HTTPoison.get(get_url) do
       {:ok, %HTTPoison.Response{status_code: status_code, body: body}} ->
         repsonse="ok"
@@ -20,16 +21,19 @@ defmodule Spaces.SessionController do
             [_,{_,access_token},_,{_,user_id},{_,team_name},{_,team_id}] = body
             conn = put_session(conn, :user, [team_name: team_name, team_id: team_id, user_id: user_id])
         end
+
       {:error, %HTTPoison.Error{reason: reason}} ->
           conn = put_flash(conn, :error, "Bad Request sent to slack")
     end
+
     render conn, "new.html"
   end
 
   def slackLogin(conn, _params) do
-    url_params = %{client_id: "35296394051.35346594944", scope: "identify", redirect_uri: session_url(conn, :new)}
+    url_params = %{client_id: System.get_env("SLACK_CLIENT_ID"), scope: "identify", redirect_uri: session_url(conn, :new)}
     redirect_url = %URI{path: "https://slack.com/oauth/authorize", query: URI.encode_query(url_params)}
     |> URI.to_string
-    redirect conn, external: redirect_url 
+
+    redirect conn, external: redirect_url
   end
 end
